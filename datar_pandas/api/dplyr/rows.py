e99d474376503a54f92559d132d6affd9f4b62b7
pwwang <pwwang@pwwang.com>
2023-09-13 23:57:01 -0500

    👽️ Align rows_*() verbs to align with dplyr 1.1.3 (pwwang/datar#188)

--- datar_pandas/api/dplyr/rows.py
+++ datar_pandas/api/dplyr/rows.py
@@ -2,13 +2,14 @@
 
 https://github.com/tidyverse/dplyr/blob/master/R/rows.R
 """
+from typing import Tuple
 import numpy as np
-from datar.core.utils import logger
+from datar.core.utils import logger, arg_match
 from datar.apis.dplyr import (
     bind_rows,
-    left_join,
     coalesce,
     rows_insert,
+    rows_append,
     rows_update,
     rows_patch,
     rows_upsert,
@@ -19,86 +20,118 @@ from ... import pandas as pd
 from ...pandas import DataFrame
 from ...common import is_scalar, setdiff
 from ...contexts import Context
-from ..tibble.verbs import rownames_to_column
 
 _meta_args = {"__ast_fallback": "normal", "__backend": "pandas"}
 
 
 @rows_insert.register(DataFrame, context=Context.EVAL, backend="pandas")
-def _rows_insert(x, y, by=None, copy=True):
+def _rows_insert(
+    x,
+    y,
+    by=None,
+    conflict="error",
+    **kwargs,
+):
+    if kwargs:  # pragma: no cover
+        raise ValueError("Unsupported arguments: %s" % kwargs.keys())
+
+    conflict = arg_match(conflict, "conflict", ["error", "ignore"])
+
     key = _rows_check_key(by, x, y)
     _rows_check_key_df(x, key, df_name="x")
     _rows_check_key_df(y, key, df_name="y")
 
-    idx = _rows_match(y[key], x[key])
-    bad = ~pd.isnull(idx)
-    if any(bad):
+    idx_x, idx_y = _rows_match(x[key], y[key])
+    if idx_x.size > 0 and conflict == "error":
         raise ValueError("Attempting to insert duplicate rows.")
 
-    return bind_rows(x, y, _copy=copy, **_meta_args)
+    idx_y = np.isin(y.index, idx_y, invert=True)
+    return bind_rows(x, y.loc[idx_y, :], **_meta_args)
+
+
+@rows_append.register(DataFrame, context=Context.EVAL, backend="pandas")
+def _rows_append(x, y, **kwargs):
+    if kwargs:  # pragma: no cover
+        raise ValueError("Unsupported arguments: %s" % kwargs.keys())
+
+    _rows_check_key_df(x, y.columns, df_name="x")
+    return bind_rows(x, y, **_meta_args)
 
 
 @rows_update.register(DataFrame, context=Context.EVAL, backend="pandas")
-def _rows_update(x, y, by=None, copy=True):
+def _rows_update(x, y, by=None, unmatched="error", **kwargs):
+    if kwargs:  # pragma: no cover
+        raise ValueError("Unsupported arguments: %s" % kwargs.keys())
+
+    unmatched = arg_match(unmatched, "unmatched", ["error", "ignore"])
+
     key = _rows_check_key(by, x, y)
     _rows_check_key_df(x, key, df_name="x")
     _rows_check_key_df(y, key, df_name="y")
 
-    idx = _rows_match(y[key], x[key])
-    bad = pd.isnull(idx)
-    if any(bad):
+    idx_x, idx_y = _rows_match(x[key], y[key])
+
+    if y.index.difference(idx_y).size > 0 and unmatched == "error":
         raise ValueError("Attempting to update missing rows.")
 
-    idx = idx.astype(int)
+    if np.unique(idx_x).size < idx_x.size:
+        raise ValueError("`y` key values must be unique.")
 
-    if copy:
-        x = x.copy()
-
-    # Join at the beginning? NaNs will be produced and dtypes will be changed
-    # in y
-    # Try it in pandas2
-    y_joined = left_join(x.loc[idx, key], y, by=key, **_meta_args).set_index(
-        idx
-    )
-
-    x.loc[idx, y.columns] = y_joined
+    x = x.copy()
+    x.loc[idx_x, y.columns] = y.loc[idx_y, :].values
     return x
 
 
 @rows_patch.register(DataFrame, context=Context.EVAL, backend="pandas")
-def _rows_patch(x, y, by=None, copy=True):
+def _rows_patch(x, y, by=None, unmatched="error", **kwargs):
+    if kwargs:  # pragma: no cover
+        raise ValueError("Unsupported arguments: %s" % kwargs.keys())
+
+    unmatched = arg_match(unmatched, "unmatched", ["error", "ignore"])
+
     key = _rows_check_key(by, x, y)
     _rows_check_key_df(x, key, df_name="x")
     _rows_check_key_df(y, key, df_name="y")
 
-    idx = _rows_match(y[key], x[key])
-    bad = pd.isnull(idx)
-    if any(bad):
+    idx_x, idx_y = _rows_match(x[key], y[key])
+
+    if idx_x.size == 0:
         raise ValueError("Attempting to patch missing rows.")
 
-    new_data = []
-    for col in y.columns:
-        new_data.append(coalesce(x.loc[idx, col].values, y[col]))
+    if y.index.difference(idx_y).size > 0 and unmatched == "error":
+        raise ValueError("`y` must contain keys that already exist in `x`.")
 
-    if copy:
-        x = x.copy()
-    x.loc[idx, y.columns] = np.array(new_data).T
-    return x
+    if np.unique(idx_x).size < idx_x.size:
+        raise ValueError("`y` key values must be unique.")
+
+    z = x.copy()
+
+    other_cols = y.columns.difference(key)
+    z.loc[idx_x, other_cols] = coalesce(
+        x.loc[idx_x, other_cols],
+        y.loc[idx_y, other_cols].set_index(idx_x),
+    )
+
+    for col in other_cols:
+        z[col] = z[col].astype(x[col].dtype)
+
+    return z
 
 
 @rows_upsert.register(DataFrame, context=Context.EVAL, backend="pandas")
-def _rows_upsert(x, y, by=None, copy=True):
+def _rows_upsert(x, y, by=None):
     key = _rows_check_key(by, x, y)
     _rows_check_key_df(x, key, df_name="x")
     _rows_check_key_df(y, key, df_name="y")
 
-    idx = _rows_match(y[key], x[key])
-    new = pd.isnull(idx)
-    # idx of x
-    idx_existing = idx[~new]
+    idx_x, idx_y = _rows_match(x[key], y[key])
 
-    x.loc[idx_existing, y.columns] = y.loc[~new].values
-    return bind_rows(x, y.loc[new], _copy=copy, **_meta_args)
+    if np.unique(idx_x).size < idx_x.size:
+        raise ValueError("`y` key values must be unique.")
+
+    x = x.copy()
+    x.loc[idx_x, y.columns] = y.loc[idx_y, :].values
+    return bind_rows(x, y.loc[~y.index.isin(idx_y)], **_meta_args)
 
 
 @rows_delete.register(DataFrame, context=Context.EVAL, backend="pandas")
@@ -106,32 +139,35 @@ def _rows_delete(
     x,
     y,
     by=None,
-    copy=True,
+    unmatched="error",
+    **kwargs,
 ):
-    key = _rows_check_key(by, x, y)
+    if kwargs:  # pragma: no cover
+        raise ValueError("Unsupported arguments: %s" % kwargs.keys())
+
+    unmatched = arg_match(unmatched, "unmatched", ["error", "ignore"])
+
+    key = _rows_check_key(by, x, y, allow_y_extra=True)
     _rows_check_key_df(x, key, df_name="x")
     _rows_check_key_df(y, key, df_name="y")
 
-    extra_cols = setdiff(y.columns, key)
+    extra_cols = y.columns.difference(key)
     if len(extra_cols) > 0:
         logger.info("Ignoring extra columns: %s", extra_cols)
 
-    idx = _rows_match(y[key], x[key])
-    bad = pd.isnull(idx)
+    idx_x, idx_y = _rows_match(x[key], y[key])
 
-    if any(bad):
+    if y.index.difference(idx_y).size > 0 and unmatched == "error":
         raise ValueError("Attempting to delete missing rows.")
 
-    if copy:
-        x = x.copy()
-
-    return x.loc[~x.index.isin(idx), :]
+    x = x.copy()
+    return x.loc[~x.index.isin(idx_x), :]
 
 
 # helpers -----------------------------------------------------------------
 
 
-def _rows_check_key(by, x, y):
+def _rows_check_key(by, x, y, allow_y_extra=False):
     """Check the key and return the valid key"""
     if by is None:
         by = y.columns[0]
@@ -145,7 +181,7 @@ def _rows_check_key(by, x, y):
             raise ValueError("`by` must be a string or a list of strings.")
 
     bad = setdiff(y.columns, x.columns)
-    if len(bad) > 0:
+    if len(bad) > 0 and not allow_y_extra:
         raise ValueError("All columns in `y` must exist in `x`.")
 
     return by
@@ -161,9 +197,22 @@ def _rows_check_key_df(df, by, df_name) -> None:
     #     raise ValueError(f"`{df_name}` key values are not unique.")
 
 
-def _rows_match(x: pd.DataFrame, y: pd.DataFrame, for_: str = "x"):
+def _rows_match(
+    x: pd.DataFrame,
+    y: pd.DataFrame,
+) -> Tuple[np.ndarray, np.ndarray]:
     """Mimic vctrs::vec_match"""
-    id_col = "__id__"
-    y_with_id = rownames_to_column(y, var=id_col, **_meta_args)
-
-    return left_join(x, y_with_id, **_meta_args)[id_col].values
+    x_id_col = "__x_id__"
+    y_id_col = "__y_id__"
+    xin = x.index.name
+    yin = y.index.name
+    x.index.name = x_id_col
+    y.index.name = y_id_col
+    xi = x.reset_index()
+    yi = y.reset_index()
+    x.index.name = xin
+    y.index.name = yin
+    merge_col = "__merge__"
+    df = xi.merge(yi, how="left", indicator=merge_col)
+    df = df[df[merge_col] == "both"]
+    return df[x_id_col].values.astype(int), df[y_id_col].values.astype(int)
--- tests/dplyr/test_rows.py
+++ tests/dplyr/test_rows.py
@@ -9,7 +9,7 @@ from datar.all import (
     NA,
     c,
     rows_insert,
-    # rows_append,
+    rows_append,
     rows_delete,
     rows_patch,
     rows_update,
@@ -23,19 +23,16 @@ def data():
     return tibble(a=seq(1, 3), b=c(letters[[0, 1]], NA), c=[0.5, 1.5, 2.5])
 
 
+# rows_insert
 def test_rows_insert(data):
     out = rows_insert(data, tibble(a=4, b="z"), by="a")
     exp = tibble(a=seq(1, 4), b=c("a", "b", NA, "z"), c=c(0.5, 1.5, 2.5, NA))
     assert_frame_equal(out, exp)
 
-    with pytest.raises(ValueError, match="insert duplicate"):
-        rows_insert(data, tibble(a=3, b="z"), by="a")
 
-
-def test_rows_insert_doesnt_allow_dup_key_values():
+def test_rows_insert_disallows_matched_keys_by_default():
     x = tibble(a=1, b=2)
     y = tibble(a=1, b=3)
-
     with pytest.raises(ValueError, match="insert duplicate"):
         rows_insert(x, y, by="a")
 
@@ -44,7 +41,19 @@ def test_rows_insert_doesnt_allow_dup_key_values():
         rows_insert(x, y, by="a")
 
 
-def test_rows_insert_allow_x_dup_keys():
+def test_rows_insert_allows_matched_keys_by_conflict_arg():
+    x = tibble(a=1, b=2)
+    y = tibble(a=1, b=3)
+    out = rows_insert(x, y, by="a", conflict="ignore")
+    assert_frame_equal(out, x)
+
+    y = tibble(a=[1, 2, 1], b=[3, 4, 5])
+    out = rows_insert(x, y, by="a", conflict="ignore")
+    exp = tibble(a=[1, 2], b=[2, 4])
+    assert_frame_equal(out, exp)
+
+
+def test_rows_insert_allows_x_dup_keys():
     x = tibble(a=[1, 1], b=[2, 3])
     y = tibble(a=2, b=4)
 
@@ -53,7 +62,7 @@ def test_rows_insert_allow_x_dup_keys():
     assert_frame_equal(out, exp)
 
 
-def test_rows_insert_allow_y_dup_keys():
+def test_rows_insert_allows_y_dup_keys():
     x = tibble(a=2, b=4)
     y = tibble(a=[1, 1], b=[2, 3])
     out = rows_insert(x, y, by="a")
@@ -61,24 +70,78 @@ def test_rows_insert_allow_y_dup_keys():
     assert_frame_equal(out, exp)
 
 
+def test_rows_insert_checks_x_y_have_by():
+    x = tibble(a=1, b=2)
+    y = tibble(a=1)
+
+    with pytest.raises(ValueError, match="All `by` columns must exist"):
+        rows_insert(x, y, by="c")
+
+    with pytest.raises(ValueError, match="All `by` columns must exist"):
+        rows_insert(x, y, by=["a", "b"])
+
+
+def test_rows_insert_conflict_arg():
+    x = tibble(a=1)
+    y = tibble(b=2)
+
+    with pytest.raises(ValueError, match="conflict"):
+        rows_insert(x, y, by="a", conflict="foo")
+
+    with pytest.raises(ValueError, match="conflict"):
+        rows_insert(x, y, by="a", conflict=1)
+
+
+# rows_append
+def test_rows_append(data):
+    x = tibble(a=1, b=2)
+    y = tibble(a=1, b=3)
+    out = rows_append(x, y)
+    exp = tibble(a=[1, 1], b=[2, 3])
+    assert_frame_equal(out, exp)
+
+    y = tibble(a=[1, 2, 1], b=[3, 4, 5])
+    out = rows_append(x, y)
+    exp = tibble(a=[1, 1, 2, 1], b=[2, 3, 4, 5])
+
+
+def test_rows_append_requires_y_cols_in_x():
+    x = tibble(a=1, b=2)
+    y = tibble(a=1, b=2, c=3)
+    with pytest.raises(ValueError):
+        rows_append(x, y)
+
+
+def test_rows_append_doesnt_require_x_cols_in_y():
+    x = tibble(a=1, b=2, c=3)
+    y = tibble(a=1, b=2)
+    out = rows_append(x, y)
+    assert_frame_equal(out, tibble(a=[1, 1], b=[2, 2], c=[3, NA]))
+
+
+# rows_update
 def test_rows_update(data):
     out = rows_update(data, tibble(a=[2, 3], b="z"), by="a")
     exp = tibble(a=seq(1, 3), b=c("a", "z", "z"), c=data.c)
     assert_frame_equal(out, exp)
-
-    # with pytest.raises(ValueError, match="update missing"):
-    #     rows_update(data, tibble(a=[2, 3], b="z"), by=["a", "b"])
 
     out = rows_update(data, tibble(b="z", a=[2, 3]), by="a")
     exp = tibble(a=seq(1, 3), b=c("a", "z", "z"), c=data.c)
     assert_frame_equal(out, exp)
 
 
-def test_rows_update_requires_y_keys_in_x():
+def test_rows_update_requires_y_keys_in_x_by_default():
     x = tibble(a=1, b=2)
     y = tibble(a=[2, 1, 3], b=[1, 1, 1])
     with pytest.raises(ValueError):
         rows_update(x, y, by="a")
+
+
+def test_rows_update_allows_y_keys_not_in_x_by_unmatched_arg():
+    x = tibble(a=1, b=2)
+    y = tibble(a=[2, 1, 3], b=[1, 1, 1])
+    out = rows_update(x, y, by="a", unmatched="ignore")
+    assert_frame_equal(out, tibble(a=1, b=1))
 
 
 def test_rows_update_allow_x_dup_keys():
@@ -93,41 +156,152 @@ def test_rows_update_disallow_y_dup_keys():
     x = tibble(a=2, b=4)
     y = tibble(a=[2, 2], b=[2, 3])
 
-    with pytest.raises(ValueError, match="Length mismatch"):
+    with pytest.raises(ValueError, match="must be unique"):
         rows_update(x, y, by="a")
 
 
+def test_rows_update_unmatched_arg():
+    x = tibble(a=1)
+    y = tibble(a=1)
+
+    with pytest.raises(ValueError, match="unmatched"):
+        rows_update(x, y, by="a", unmatched="foo")
+
+    with pytest.raises(ValueError, match="unmatched"):
+        rows_update(x, y, by="a", unmatched=1)
+
+
+# rows_patch
 def test_rows_patch(data):
     out = rows_patch(data, tibble(a=[2, 3], b="z"), by="a")
     exp = tibble(a=seq(1, 3), b=c("a", "b", "z"), c=data.c)
     assert_frame_equal(out, exp)
-
-    with pytest.raises(ValueError, match="patch missing"):
-        rows_patch(data, tibble(a=[2, 3], b="z"), by=["a", "b"])
 
     out = rows_patch(data, tibble(b="z", a=[2, 3]), by="a")
     exp = tibble(a=seq(1, 3), b=c("a", "b", "z"), c=data.c)
     assert_frame_equal(out, exp)
 
 
+def test_rows_patch_requires_y_keys_in_x_by_default():
+    x = tibble(a=1, b=2)
+    y = tibble(a=[2, 1, 3], b=[1, 1, 1])
+    with pytest.raises(ValueError):
+        rows_patch(x, y, by="a")
+
+
+def test_rows_patch_allows_y_keys_not_in_x_by_unmatched_arg():
+    x = tibble(a=1, b=NA)
+    y = tibble(a=[2, 1, 3], b=[1, 1, 1])
+    out = rows_patch(x, y, by="a", unmatched="ignore")
+    assert_frame_equal(out, tibble(a=1, b=1.))
+
+
+def test_rows_patch_allow_x_dup_keys():
+    x = tibble(a=[1, 2, 1, 3], b=[NA, 3, 4, NA], c=letters[:4])
+    y = tibble(a=[1, 3], b=[99, 88])
+    out = rows_patch(x, y, by="a")
+    exp = tibble(a=[1, 2, 1, 3], b=[99., 3, 4, 88], c=letters[:4])
+    assert_frame_equal(out, exp)
+
+
+def test_rows_patch_disallow_y_dup_keys():
+    x = tibble(a=2, b=4)
+    y = tibble(a=[2, 2], b=[2, 3])
+
+    with pytest.raises(ValueError, match="must be unique"):
+        rows_patch(x, y, by="a")
+
+
+def test_rows_patch_unmatched_arg():
+    x = tibble(a=1)
+    y = tibble(a=1)
+
+    with pytest.raises(ValueError, match="unmatched"):
+        rows_patch(x, y, by="a", unmatched="foo")
+
+    with pytest.raises(ValueError, match="unmatched"):
+        rows_patch(x, y, by="a", unmatched=1)
+
+
+# rows_upsert
 def test_rows_upsert(data):
     out = rows_upsert(data, tibble(a=[2, 3, 4], b="z"), by="a")
     exp = tibble(a=seq(1, 4), b=c("a", "z", "z", "z"), c=c(data.c.values, NA))
     assert_frame_equal(out, exp)
 
 
+def test_rows_upsert_allow_x_dup_keys():
+    x = tibble(a=[1, 2, 1, 3], b=[NA, 3, 4, NA], c=letters[:4])
+    y = tibble(a=[1, 3, 4], b=[99, 88, 100])
+    out = rows_upsert(x, y, by="a")
+    exp = tibble(
+        a=[1, 2, 1, 3, 4],
+        b=[99., 3, 99, 88, 100],
+        c=c(letters[:4], NA),
+    )
+    assert_frame_equal(out, exp)
+
+
+def test_rows_upsert_disallow_y_dup_keys():
+    x = tibble(a=2, b=4)
+    y = tibble(a=[2, 2], b=[2, 3])
+
+    with pytest.raises(ValueError, match="must be unique"):
+        rows_upsert(x, y, by="a")
+
+
+# rows_delete
 def test_rows_delete(data):
     out = data >> rows_delete(tibble(a=[2, 3]), by="a")
     assert_frame_equal(out, data.iloc[[0], :])
-
-    with pytest.raises(ValueError, match="delete missing"):
-        data >> rows_delete(tibble(a=[2, 3, 4]), by="a")
 
     out = data >> rows_delete(tibble(a=[2, 3], b="b"), by="a")
     assert_frame_equal(out, data.iloc[[0], :])
 
     with pytest.raises(ValueError, match="delete missing"):
         data >> rows_delete(tibble(a=[2, 3], b="b"), by=["a", "b"])
+
+
+def test_rows_delete_ignores_extra_y_cols(caplog):
+    x = tibble(a=1)
+    y = tibble(a=1, b=2)
+    out = rows_delete(x, y)
+    assert "Ignoring extra columns" in caplog.text
+    assert out.shape[0] == 0
+
+    out = rows_delete(x, y, by="a")
+    assert "Ignoring extra columns" in caplog.text
+    assert out.shape[0] == 0
+
+
+def test_rows_delete_requires_y_keys_in_x_by_default():
+    x = tibble(a=1, b=2)
+    y = tibble(a=[2, 1, 3], b=[1, 1, 1])
+    with pytest.raises(ValueError):
+        rows_delete(x, y, by="a")
+
+
+def test_rows_delete_allows_y_keys_not_in_x_by_unmatched_arg():
+    x = tibble(a=1, b=2)
+    y = tibble(a=[2, 1, 3], b=[1, 1, 1])
+    out = rows_delete(x, y, by="a", unmatched="ignore")
+    assert out.shape[0] == 0
+
+
+def test_rows_delete_allow_x_dup_keys():
+    x = tibble(a=[1, 2, 1, 3], b=[NA, 3, 4, NA], c=letters[:4])
+    y = tibble(a=[1, 3])
+    out = rows_delete(x, y, by="a")
+    exp = x.iloc[[1], :]
+    assert_frame_equal(out, exp)
+
+
+def test_rows_delete_allow_y_dup_keys():
+    x = tibble(a=[1, 2, 3], b=[4, 5, 6])
+    y = tibble(a=[1, 1])
+    out = rows_delete(x, y, by="a")
+    exp = x.iloc[[1, 2], :]
+    assert_frame_equal(out, exp)
 
 
 def test_rows_errors(data):
