52899c1318c294d400721bac01b6aacd820ed528
pwwang <pwwang@pwwang.com>
2025-10-26 19:43:32 -0700

    test: update exception type in test_no_such
    - Changed exception raised in test_no_such from ImportError to ResultError

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,10 @@
 import pytest
+from simplug import ResultError
 from datar_pandas.pandas import DataFrame
 
 
 def test_no_such():
-    with pytest.raises(ImportError):
+    with pytest.raises(ResultError):
         from datar.data import nosuch  # noqa: F401
 
 
